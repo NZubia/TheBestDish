c41827587968272b328d3e7659ad0ac3f8be54f5
Normando Ali Zubia Hernandez <nzubia@blacklabs.mx>
2019-10-16 19:28:17 -0600

    Se corrige error de cors:

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,6 +30,8 @@ mongoose.connection.on("open", function () {
 
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
